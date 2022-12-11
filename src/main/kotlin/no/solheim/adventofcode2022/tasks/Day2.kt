3c7bace0d301efef572d3587ae8570275a219d22
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2022-12-11 01:22:30 +0100

    Day2

--- src/main/kotlin/no/solheim/adventofcode2022/tasks/Day2.kt
+++ src/main/kotlin/no/solheim/adventofcode2022/tasks/Day2.kt
@@ -3,38 +3,23 @@ package no.solheim.adventofcode2022.tasks
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Component
+import no.solheim.adventofcode2022.tasks.Move.*
 
-enum class Move(val score: Int) {
-    Rock(1),
-    Paper(2),
-    Scissors(3)
-}
+enum class Move(val score: Int) { R(1), P(2), S(3) }
 
-class Game(val p1Move: Move, val p2Move: Move) {
-    val p1Score get(): Int {
-        return 0
-    }
+val moves = mapOf("A" to R, "X" to R, "B" to P, "Y" to P, "C" to S, "Z" to S)
+
+fun score(player: Move, opponent: Move): Int {
+    return 0
 }
 
 @Component
 class Day2(@Value("classpath:/inputs/day_2.txt") val input: Resource) : DayTask {
     override fun run() {
         val input = this.input.file.readText()
-        input.split("\n").map { 
-            val (him, me) = it.split(" ") 
-            val hisMove = when(him) {
-                "A" -> Move.Rock
-                "B" -> Move.Paper
-                else -> Move.Scissors
-            }
-            val myMove = when(me) {
-                "X" -> Move.Rock
-                "Y" -> Move.Paper
-                else -> Move.Scissors
-            }
-
-            val game = Game(myMove, hisMove)
-            println(game.p1Score)
+        input.split("\n").map {
+            val (him, me) = it.split(" ").map { moves[it]!! }
+            println(score(me, him))
         }
     }
 }
