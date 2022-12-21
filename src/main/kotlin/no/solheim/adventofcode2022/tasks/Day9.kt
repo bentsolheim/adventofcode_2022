d2dd022155827f7659d62934afd83e6adb0ae7d0
Bent Andre Solheim <BentAndre.Solheim@skatteetaten.no>
2022-12-21 21:31:41 +0100

    Part 1 - the rope moves after head, but not according to spec

--- src/main/kotlin/no/solheim/adventofcode2022/tasks/Day9.kt
+++ src/main/kotlin/no/solheim/adventofcode2022/tasks/Day9.kt
@@ -1,39 +1,94 @@
 package no.solheim.adventofcode2022.tasks
 
 import kotlin.math.abs
+import kotlin.math.max
 import nonBlankLines
 
-class Day9(val input: String) {
+typealias Point = Pair<Int, Int>
 
-    var head = 0 to 0
-    var tail = 0 to 0
-    val tailTouches = mutableSetOf<Pair<Int, Int>>()
+val Point.x: Int get() = this.first
+val Point.y: Int get() = this.second
+
+class Day9(val input: String, val partCount: Int = 2) {
+
+    // var head = 0 to 0
+    var parts = (0..partCount - 1).map { Point(0, 0) }.toMutableList()
+
+    // var tail = 0 to 0
+    val tailTouches = mutableSetOf<Point>()
 
     fun getTailTouchCount(): Int {
         val moves = input.nonBlankLines().map { it.split(" ") }.map { it[0] to it[1].toInt() }
         moves.forEach { (dir, count) ->
             repeat(count) {
-                val (hdx, hdy) = when (dir) {
+                val (hdxi, hdyi) = when (dir) {
                     "R" -> 1 to 0
                     "U" -> 0 to 1
                     "D" -> 0 to -1
                     else -> -1 to 0
                 }
-                head = head.run { first + hdx to second + hdy }
-                if (abs(head.first - tail.first) > 1 || abs(head.second - tail.second) > 1) {
-                    tail = head.copy(first = head.first + (hdx * -1), second = head.second + (hdy * -1))
-                }
-                tailTouches.add(tail)
+                var head = parts[0]
+                val newHead = head.run { x + hdxi to y + hdyi }
+                val hdx = newHead.x - head.x
+                val hdy = newHead.y - head.y
+                parts[0] = newHead
+                extracted(1, hdx, hdy)
+                printParts()
             }
         }
+        println(parts)
+        printTailTouches()
         return tailTouches.size
     }
 
+    private fun extracted(tailIndex: Int, hdx: Int, hdy: Int) {
+        val headIndex = tailIndex - 1
+        val head = parts[headIndex]
+        val tail = parts[tailIndex]
+        val dx = tail.x - head.x
+        val dy = tail.y - head.y
+        if (abs(dx) > 1 || abs(dy) > 1) {
+            val newTail = head.copy(
+                first = head.x + (hdx * -1),
+                second = head.y + (hdy * -1)
+            )
+            val tdx = newTail.x - tail.x
+            val tdy = newTail.y - tail.y
+            parts[tailIndex] = newTail
+
+            if (tailIndex != parts.size - 1) {
+                extracted(tailIndex + 1, tdx, tdy)
+            }
+        }
+        if (tailIndex == parts.size - 1) {
+            tailTouches.add(tail)
+        }
+    }
+
     private fun printTailTouches() {
-        val maxX = tailTouches.maxBy { it.first }.first + 1
-        val maxY = tailTouches.maxBy { it.second }.second + 1
+        val maxX = tailTouches.run { maxBy { (max(it.x, abs(it.x))) }.x * 2 } + 1
+        val maxY = tailTouches.run { maxBy { (max(it.y, abs(it.y))) }.y * 2 } + 1
+        val xOffset = maxX / 2
+        val yOffset = maxY / 2
         val coordinates = (0..maxY).map { (0..maxX).map { "." }.toMutableList() }.toMutableList()
-        tailTouches.forEach { (x, y) -> coordinates[y][x] = "#" }
+        tailTouches.forEach { (x, y) -> coordinates[yOffset + y][xOffset + x] = "#" }
+        coordinates[yOffset][xOffset] = "s"
+        coordinates.reversed().forEach { println(it.joinToString("")) }
+    }
+
+    private fun printParts() {
+        val maxX = 30
+        val maxY = 20
+        val xOffset = maxX / 2
+        val yOffset = maxY / 2
+        val coordinates = (0..maxY).map { (0..maxX).map { "." }.toMutableList() }.toMutableList()
+        parts.reversed().forEachIndexed() { i, (x, y) ->
+            val o = (parts.size - i).let { if (it == 1) "H" else "${it-1}" }
+            coordinates[yOffset + y][xOffset + x] = o
+        }
+        tailTouches.forEach { (x, y) -> coordinates[yOffset + y][xOffset + x] = "#" }
+        coordinates[yOffset][xOffset] = "s"
+        println("\n")
         coordinates.reversed().forEach { println(it.joinToString("")) }
     }
 }
--- src/test/kotlin/no/solheim/adventofcode2022/tasks/Day9Test.kt
+++ src/test/kotlin/no/solheim/adventofcode2022/tasks/Day9Test.kt
@@ -16,13 +16,27 @@ L 5
 R 2        
     """.trim()
 
+    val example2 = """
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20        
+    """.trim()
+
     @Test
     fun example() {
-        assertThat(Day9(example).getTailTouchCount()).isEqualTo(13)
+        // assertThat(Day9(example, 2).getTailTouchCount()).isEqualTo(13)
+        Day9(example2, 10).getTailTouchCount()
     }
 
     @Test
     fun input() {
-        println(Day9(input).getTailTouchCount())
+        val message = Day9(input).getTailTouchCount()
+        println(message)
+        assertThat(message).isEqualTo(6354)
     }
 }
